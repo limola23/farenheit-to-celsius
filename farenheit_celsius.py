90706b3296700dc14117472589f85cf819b11c90
limola23 <molanolili23@gmail.com>
2023-03-19 21:39:45 -0500

    conversor farenheit to celsius

---
+++ farenheit_celsius.py
@@ -0,0 +1,5 @@
+print("¡Hola! Bienvenido a mi conversor de Farenheit a Celsius")
+grados_farenheit = float (input("Ingresa los grados farenheit "))
+grados_celsius = (grados_farenheit - 32) * 5/9
+
+print("El resultado es: {} " .format(grados_celsius))
